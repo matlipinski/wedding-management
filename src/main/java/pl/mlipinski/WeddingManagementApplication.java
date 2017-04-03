2f233cdd34d9ae027acbe494242e4107c0fcf3dc
Mateusz Lipiński <mateusz.lipinski@comarch.pl>
2017-04-03 15:23:24 +0200

    Initial project structure

---
+++ src/main/java/pl/mlipinski/WeddingManagementApplication.java
@@ -0,0 +1,12 @@
+package pl.mlipinski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingManagementApplication.class, args);
+	}
+}
